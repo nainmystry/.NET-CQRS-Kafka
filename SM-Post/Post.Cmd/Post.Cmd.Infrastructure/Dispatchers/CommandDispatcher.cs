1ad40762e22bb4eafed24ed1deeb594c60c458fd
Nayan <nynmistry08@gmail.com>
2025-07-13 13:22:16 +0530

    Implemented Basic Mediator Pattern using Command Dispatcher to register & handle commands

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,31 @@
+﻿using CQRS.Core.Commands;
+using CQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Infrastructure.Dispatchers;
+
+public class CommandDispatcher : ICommandDispatcher
+{
+    private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new ();
+    
+    public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+    {
+        if (_handlers.ContainsKey(typeof(T))) 
+        {
+            throw new Exception("Command already registered");
+        }
+
+        _handlers.Add(typeof(T), x => handler((T)x));
+    }
+
+    public async Task SendAsync(BaseCommand command)
+    {
+        if (_handlers.TryGetValue(command.GetType(), out Func<BaseCommand, Task> handler))
+        {
+            await handler(command);
+        }
+        else
+        {
+            throw new Exception($"No handler registered for command {command.GetType().Name}");
+        }
+    }
+}
